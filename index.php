b7f28b3fcc288c7ebb9382d43a0a72d6cd477b29
AlexHorodnic <alexhorodnic@protonmail.com>
2021-12-07 17:42:54 +0200

    created login/register page using php connected to mysql database

---
+++ index.php
@@ -0,0 +1,228 @@
+<?php
+// Initialize the session
+session_start();
+ 
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>HomeXpert</title>
+    <link rel="stylesheet" href="index.css">
+    <link rel="icon" href="favicon.ico" type="ico">
+  </head>
+  <body>
+      <div class="container-fluid">
+          <div class="row row1 fixed-top" style="background-color: white;">
+              <div class="col-12">
+                <nav class="navbar navbar-expand-lg navbar-light">
+                    <a class="navbar-brand" href="index.html" style="color: #d35400; margin-right: 250px; font-size: 35px; font-weight: bolder;">HomeXpert</a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                      <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                      <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                          <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle active" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                              Design
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                              <a class="dropdown-item" href="#">Logo Design</a>
+                            </div>
+                          </li>
+                          <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle active" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                              Video & Audio
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                              <a class="dropdown-item" href="#">Video Editing</a>
+                            </div>
+                          </li>
+                          <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle active" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                              Development & IT
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                              <a class="dropdown-item" href="#">Online Coding Lessons</a>
+                            </div>
+                          </li>
+                        <li class="nav-item dropdown">
+                          <a class="nav-link dropdown-toggle active" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Marketing
+                          </a>
+                          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="#">Social Media Advertising</a>
+                          </div>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle active" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                              Writing & Translation
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                              <a class="dropdown-item" href="#">Book Editing</a>
+                            </div>
+                          </li>
+                      </ul>
+                    </div>
+                    <a href="logout.php" style="color: #d35400; font-size: 25px; font-weight: bolder;">Log Out</a>
+                  </nav>
+              </div>
+          </div>
+          <div class="row row2" style="align-items: center;  text-align: center; justify-content: center; height: 700px;">
+            <div class="col-4 align-content-center">
+                <div style="font-size: 50px;">Join the world's work marketplace</div><br>
+                <div style="font-size: 20px;">Find great talent. Build your business. Take your career to the next level.</div><br>
+                <div style="font-size: 20px; font-weight: bolder; color: #a0ac94;">Trusted by</div>
+                <img src="img/microsoft.77d03cc.svg" id="trusted">
+                <img src="img/airbnb.2baea51.svg" id="trusted">
+                <img src="img/bissell.dcb03c9.svg" id="trusted">
+                <img src="img/godaddy.75298df.svg" id="trusted">
+            </div>
+            <div class="col-4 align-content-center">
+              <img src="img/1.jpg" class="img-fluid">
+            </div>
+          </div>
+          <div class="row" style="justify-content: center; height: 600px;">
+            <div class="col-4">
+              <div style="font-size: 40px;">1. Browse</div>
+              <div style="padding-left: 30px; font-size: 20px;">
+                Find the type of work you need, clearly defined and ready to start.</div>
+                <hr style="border: 1px solid #d35400;">
+                <div style="font-size: 40px;">2. Buy</div>
+              <div style="padding-left: 30px; font-size: 20px;">
+                Work begins as soon as you purchase and provide requirements.</div>
+                <hr style="border: 1px solid #d35400;">
+                <div style="font-size: 40px;">3. Approve</div>
+              <div style="padding-left: 30px; font-size: 20px;">
+                Receive and review your project. The professional will be paid once you’ve approved it.
+                </div>
+            </div>
+            <div class="col-4">
+
+            </div>
+          </div>
+          <div class="row row3" style="justify-content: center; padding-bottom: 50px;">
+            <div class="col-4">
+              <img src="img/2.jpg" class="img-fluid" style="border-radius: 5%;">
+            </div>
+            <div class="col-4" style="background-color: f39c12; border-radius: 5%; background-image: linear-gradient(#f39c12, #d35400);">
+              <div class="row" style="color: white; font-size: 30px; padding-top: 30px; padding-left: 20px;">
+                <div class="col-12" style="background-color: #d35400;">For talent</div>
+              </div>
+              <div class="row" style="color: white; font-size: 60px; padding-top: 20px; padding-left: 20px;">
+                <div class="col-12">Find Great <br> Work</div>
+              </div>
+              <div class="row" style="color: white; font-size: 20px; padding-top: 20px; padding-left: 20px; height: 150px;">
+                <div class="col-12">Meet clients you’re excited to work with and take your career or business to new heights.</div>
+              </div>
+              <div class="row">
+                <div class="col-12">
+                  <hr style="border: 1px solid white;">
+                </div>
+              </div>
+              <div class="row" style="color: white; font-size: 20px; font-weight: bolder; padding-top: 10px; padding-left: 20px;">
+                <div class="col-4">
+                  Find opportunities for every stage of your freelance career
+                </div>
+                <div class="col-4">
+                  Control when, where, and how you work
+                </div>
+                <div class="col-4">
+                  Explore different ways to earn
+                </div>
+              </div>
+              <div class="row" style="padding-top: 30px; padding-left: 20px;">
+                <div class="col-12">
+                  <button type="button" class="btn btn-outline-light">Find Opportunities</button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="row rowfooter" style="background-color: #d35400; height: 310px;  justify-content: center; color: white; padding-top: 30px" >
+              <div class="col-2">
+                <div>For Clients</div><br>
+                <div><a href="#">⮞ How to Hire</a></div>
+                <div><a href="#">⮞ Talent Marketplace</a></div>
+                <div><a href="#">⮞ Project Catalog</a></div>
+                <div><a href="#">⮞ Talent Scout</a></div>
+                <div><a href="#">⮞ Enterprise</a></div>
+                <div><a href="#">⮞ Payroll Services</a></div>
+                <div><a href="#">⮞ Direct Contracts</a></div>
+                <div><a href="#">⮞ Hire Worldwide</a></div>
+                <div><a href="#">⮞ Hire in the USA</a></div>
+              </div>
+              <div class="col-2">
+                <div>For Talent</div><br>
+                <div><a href="#">⮞ How to Find Work</a></div>
+                <div><a href="#">⮞ Direct Contracts</a></div>
+                <div><a href="#">⮞ Find Freelance Jobs Worldwide</a></div>
+                <div><a href="#">⮞ Find Freelance Jobs in the USA</a></div>
+              </div>
+              <div class="col-2">
+                <div>Resources</div><br>
+                <div><a href="#">⮞ Help & Support</a></div>
+                <div><a href="#">⮞ Success Stories</a></div>
+                <div><a href="#">⮞ Reviews</a></div>
+                <div><a href="#">⮞ Blog</a></div>
+                <div><a href="#">⮞ Community</a></div>
+                <div><a href="#">⮞ Affiliate Program</a></div>
+              </div>
+              <div class="col-2">
+                <div>Company</div><br>
+                <div><a href="#">⮞ About Us</a></div>
+                <div><a href="#">⮞ Leadership</a></div>
+                <div><a href="#">⮞ Investor Relations</a></div>
+                <div><a href="#">⮞ Careers</a></div>
+                <div><a href="#">⮞ Our Impact</a></div>
+                <div><a href="#">⮞ Press</a></div>
+                <div><a href="#">⮞ Contact Us</a></div>
+                <div><a href="#">⮞ Trust, Safety & Security</a></div>
+                <div><a href="#">⮞ UK Modern Slavery Statement</a></div>
+              </div>
+          </div> 
+          <div class="row rowfooter" style="background-color: #d35400; justify-content: center;">
+            <div class="col-8">
+              <hr style="border: 1px solid white;">
+            </div>
+          </div>
+          <div class="row rowfooter" style="background-color: #d35400; height: 40px;">
+            <div class="col-2">
+            </div>
+            <div class="col-2">
+              <div>© 2015 - 2021 HomeXpert Global Inc.</div>
+            </div>
+            <div class="col-6">
+              <a href="#" id="paddedlinks">Privacy</a>
+              <a href="#" id="paddedlinks">Policy</a>
+              <a href="#" id="paddedlinks">Accessibility</a>
+              <a href="#" id="paddedlinks" style="float: right;">HomeXpert</a>
+            </div>
+            <div class="col-2">
+            </div>
+          </div>
+      </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+  <style>
+    @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap');
+    </style>
+</html>
---
+++ login.php
@@ -0,0 +1,214 @@
+<?php
+// Initialize the session
+session_start();
+ 
+// Check if the user is already logged in, if yes then redirect him to welcome page
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+    header("location: index.php");
+    exit;
+}
+ 
+// Include config file
+require_once "config.php";
+ 
+// Define variables and initialize with empty values
+$username = $password = "";
+$username_err = $password_err = $login_err = "";
+ 
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 
+    // Check if username is empty
+    if(empty(trim($_POST["username"]))){
+        $username_err = "Please enter username.";
+    } else{
+        $username = trim($_POST["username"]);
+    }
+    
+    // Check if password is empty
+    if(empty(trim($_POST["password"]))){
+        $password_err = "Please enter your password.";
+    } else{
+        $password = trim($_POST["password"]);
+    }
+    
+    // Validate credentials
+    if(empty($username_err) && empty($password_err)){
+        // Prepare a select statement
+        $sql = "SELECT id, username, password FROM users WHERE username = ?";
+        
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "s", $param_username);
+            
+            // Set parameters
+            $param_username = $username;
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                // Store result
+                mysqli_stmt_store_result($stmt);
+                
+                // Check if username exists, if yes then verify password
+                if(mysqli_stmt_num_rows($stmt) == 1){                    
+                    // Bind result variables
+                    mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
+                    if(mysqli_stmt_fetch($stmt)){
+                        if(password_verify($password, $hashed_password)){
+                            // Password is correct, so start a new session
+                            session_start();
+                            
+                            // Store data in session variables
+                            $_SESSION["loggedin"] = true;
+                            $_SESSION["id"] = $id;
+                            $_SESSION["username"] = $username;                            
+                            
+                            // Redirect user to welcome page
+                            header("location: index.php");
+                        } else{
+                            // Password is not valid, display a generic error message
+                            $login_err = "Invalid username or password.";
+                        }
+                    }
+                } else{
+                    // Username doesn't exist, display a generic error message
+                    $login_err = "Invalid username or password.";
+                }
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+
+            // Close statement
+            mysqli_stmt_close($stmt);
+        }
+    }
+    
+    // Close connection
+    mysqli_close($link);
+}
+?>
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        *{ font-family: 'Open Sans', sans-serif; }
+        .wrapper{ width: 360px; padding: 20px; }
+        @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap');
+    </style>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+    <div class="container-fluid">
+    <div class="row" style="background-color: white; justify-content: center; border-bottom: 2px solid #fc7930;">
+              <div class="col-2">
+                <nav class="navbar navbar-expand-lg navbar-light">
+                    <a class="navbar-brand" href="login.php" style="color: #d35400; margin-right: 250px; font-size: 35px;">HomeXpert</a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                      <span class="navbar-toggler-icon"></span>
+                    </button>
+                  </nav>
+              </div>
+          </div>
+    </div>
+    <div class="container" >
+        <div class="row" style="align-items: center; justify-content: center; height:600px; ">
+        <div class="wrapper">
+            <h2 style="color: #d35400;">Login</h2>
+            <p>Please fill in your credentials to login.</p>
+
+            <?php 
+            if(!empty($login_err)){
+                echo '<div class="alert alert-danger">' . $login_err . '</div>';
+            }        
+            ?>
+
+            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                <div class="form-group">
+                    <label>Username</label>
+                    <input type="text" name="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
+                    <span class="invalid-feedback"><?php echo $username_err; ?></span>
+                </div>    
+                <div class="form-group">
+                    <label>Password</label>
+                    <input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>">
+                    <span class="invalid-feedback"><?php echo $password_err; ?></span>
+                </div>
+                <div class="form-group">
+                    <input type="submit" class="btn btn-primary" value="Login" style="background-color: #d35400; border: none;">
+                </div>
+                <p>Don't have an account? <a href="register.php" style="color: #d35400;">Sign up now</a>.</p>
+            </form>
+        </div>
+        </div>
+    </div>
+    <div class="container-fluid">
+    <div class="row rowfooter" style="background-color: #d35400; height: 310px;  justify-content: center; color: white; padding-top: 30px" >
+              <div class="col-2">
+                <div>For Clients</div><br>
+                <div><a href="#">⮞ How to Hire</a></div>
+                <div><a href="#">⮞ Talent Marketplace</a></div>
+                <div><a href="#">⮞ Project Catalog</a></div>
+                <div><a href="#">⮞ Talent Scout</a></div>
+                <div><a href="#">⮞ Enterprise</a></div>
+                <div><a href="#">⮞ Payroll Services</a></div>
+                <div><a href="#">⮞ Direct Contracts</a></div>
+                <div><a href="#">⮞ Hire Worldwide</a></div>
+                <div><a href="#">⮞ Hire in the USA</a></div>
+              </div>
+              <div class="col-2">
+                <div>For Talent</div><br>
+                <div><a href="#">⮞ How to Find Work</a></div>
+                <div><a href="#">⮞ Direct Contracts</a></div>
+                <div><a href="#">⮞ Find Freelance Jobs Worldwide</a></div>
+                <div><a href="#">⮞ Find Freelance Jobs in the USA</a></div>
+              </div>
+              <div class="col-2">
+                <div>Resources</div><br>
+                <div><a href="#">⮞ Help & Support</a></div>
+                <div><a href="#">⮞ Success Stories</a></div>
+                <div><a href="#">⮞ Reviews</a></div>
+                <div><a href="#">⮞ Blog</a></div>
+                <div><a href="#">⮞ Community</a></div>
+                <div><a href="#">⮞ Affiliate Program</a></div>
+              </div>
+              <div class="col-2">
+                <div>Company</div><br>
+                <div><a href="#">⮞ About Us</a></div>
+                <div><a href="#">⮞ Leadership</a></div>
+                <div><a href="#">⮞ Investor Relations</a></div>
+                <div><a href="#">⮞ Careers</a></div>
+                <div><a href="#">⮞ Our Impact</a></div>
+                <div><a href="#">⮞ Press</a></div>
+                <div><a href="#">⮞ Contact Us</a></div>
+                <div><a href="#">⮞ Trust, Safety & Security</a></div>
+                <div><a href="#">⮞ UK Modern Slavery Statement</a></div>
+              </div>
+          </div> 
+          <div class="row rowfooter" style="background-color: #d35400; justify-content: center;">
+            <div class="col-8">
+              <hr style="border: 1px solid white;">
+            </div>
+          </div>
+          <div class="row rowfooter" style="background-color: #d35400; height: 40px;">
+            <div class="col-2">
+            </div>
+            <div class="col-2">
+              <div>© 2015 - 2021 HomeXpert Global Inc.</div>
+            </div>
+            <div class="col-6">
+              <a href="#" id="paddedlinks">Privacy</a>
+              <a href="#" id="paddedlinks">Policy</a>
+              <a href="#" id="paddedlinks">Accessibility</a>
+              <a href="#" id="paddedlinks" style="float: right;">HomeXpert</a>
+            </div>
+            <div class="col-2">
+            </div>
+          </div>
+    </div>
+    
+</body>
+</html>
